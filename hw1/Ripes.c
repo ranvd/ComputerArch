c66a3d42a4577898090f95b1e41d4ed0775438b0
minired <jordan1415926535@gmail.com>
2023-10-02 18:14:16 +0800

    Add hw1 sine with rv32i

---
+++ hw1/Ripes.c
@@ -0,0 +1,214 @@
+
+asm("li sp, 0x7ffffff0");  // SP set to 1 MB
+asm("jal main");         // call main
+asm("li a7, 2");        // prepare ecall exit
+asm("ecall");            // now your simulator should stop
+asm("li a7, 10");
+asm("ecall");
+typedef long long int64_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+uint32_t count_leading_zeros(uint32_t x) {
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+
+    /* count ones (population count) */
+    x -= ((x >> 1) & 0x55555555);
+    x = ((x >> 2) & 0x33333333) + (x & 0x33333333);
+    x = ((x >> 4) + x) & 0x0f0f0f0f;
+    x += (x >> 8);
+    x += (x >> 16);
+
+    return (32 - (x & 0x7f));
+}
+
+static inline int64_t getbit(int64_t value, int n) { return (value >> n) & 1; }
+/* 24 bit mantissa multiply */
+static int32_t imul24(int32_t a, int32_t b) {
+    uint32_t r = 0;
+    for (; b; b >>= 1)
+        r = (r >> 1) + (a & -getbit(b, 0));
+    return r;
+}
+
+/* float32 multiply */
+int fmul32(int a, int b) {
+    /* TODO: Special values like NaN and INF */
+    int32_t ia = *(int32_t *)&a, ib = *(int32_t *)&b;
+    if (ia == 0 || ib == 0) return 0;
+
+    /* mantissa */
+    int32_t ma = (ia & 0x7FFFFF) | 0x800000;
+    int32_t mb = (ib & 0x7FFFFF) | 0x800000;
+
+    int32_t sea = ia & 0xFF800000;
+    int32_t seb = ib & 0xFF800000;
+
+    /* result of mantissa */
+    int32_t m = imul24(ma, mb);
+    int32_t mshift = getbit(m, 24);
+    m >>= mshift;
+
+    int32_t r = ((sea - 0x3f800000 + seb) & 0xFF800000) +
+                (m & (0x7fffff | mshift << 23));
+    int32_t ovfl = (r ^ seb ^ sea) >> 31;
+    r = r ^ ((r ^ 0x7f800000) & ovfl);
+    return r;
+}
+
+/* 24 bit mantissa divide */
+static int32_t idiv24(int32_t a, int32_t b) {
+    uint32_t r = 0;
+    for (int i = 0; i < 32; i++) {
+        a -= b;
+        r = (r << 1) | a >= 0;
+        a = (a + (b & -(a < 0))) << 1;
+    }
+
+    return r;
+}
+
+int fdiv32(int a, int b) {
+    int32_t ia = *(int32_t *)&a, ib = *(int32_t *)&b;
+    if (a == 0) return a;
+    if (b == 0) return 0x7f800000;
+    /* mantissa */
+    int32_t ma = (ia & 0x7FFFFF) | 0x800000;
+    int32_t mb = (ib & 0x7FFFFF) | 0x800000;
+
+    /* sign and exponent */
+    int32_t sea = ia & 0xFF800000;
+    int32_t seb = ib & 0xFF800000;
+
+    /* result of mantissa */
+    int32_t m = idiv24(ma, mb);
+    int32_t mshift = !getbit(m, 31);
+    m <<= mshift;
+
+    int32_t r = ((sea - seb + 0x3f800000) - (0x800000 & -mshift)) |
+                (m & 0x7fffff00) >> 8;
+    int32_t ovfl = (sea ^ seb ^ r) >> 31;
+    r = r ^ ((r ^ 0x7f800000) & ovfl);
+
+    return r;
+}
+
+#define iswap(x, y) ((x) ^= (y), (y) ^= (x), (x) ^= (y))
+
+int fadd32(int a, int b) {
+    int32_t ia = *(int32_t *)&a, ib = *(int32_t *)&b;
+
+    int32_t cmp_a = ia & 0x7fffffff;
+    int32_t cmp_b = ib & 0x7fffffff;
+
+    if (cmp_a < cmp_b)
+        iswap(ia, ib);
+    /* exponent */
+    int32_t ea = (ia >> 23) & 0xff;
+    int32_t eb = (ib >> 23) & 0xff;
+
+    /* mantissa */
+    int32_t ma = ia & 0x7fffff | 0x800000;
+    int32_t mb = ib & 0x7fffff | 0x800000;
+
+    int32_t align = (ea - eb > 24) ? 24 : (ea - eb);
+
+    mb >>= align;
+    if ((ia ^ ib) >> 31) {
+        ma -= mb;
+    } else {
+        ma += mb;
+    }
+
+    int32_t clz = count_leading_zeros(ma);
+    int32_t shift = 0;
+    if (clz <= 8) {
+        shift = 8 - clz;
+        ma >>= shift;
+        ea += shift;
+    } else {
+        shift = clz - 8;
+        ma <<= shift;
+        ea -= shift;
+    }
+
+    int32_t r = ia & 0x80000000 | ea << 23 | ma & 0x7fffff;
+    return r;
+}
+
+int f2i32(int x) {
+    int32_t a = *(int *)&x;
+    int32_t ma = (a & 0x7FFFFF) | 0x800000;
+    int32_t ea = ((a >> 23) & 0xFF) - 127;
+    if (ea < 0)
+        return 0;
+    else if (ea <= 23)
+        ma >>= (23 - ea);
+    else
+        ma <<= (ea - 23);
+
+    return ma;
+}
+
+int i2f32(int x) {
+    if (x == 0) return 0;
+
+    int32_t s = x & 0x80000000;
+    if (s) x = -x;
+
+    int32_t clz = count_leading_zeros(x);
+    int32_t e = 31 - clz + 127;
+
+    if (clz <= 8) {
+        x >>= 8 - clz;
+    } else {
+        x <<= clz - 8;
+    }
+
+    int r = s | e << 23 | x & 0x7fffff;
+    return r;
+}
+
+int myPow(int x, int n) {
+    int r = 0x3f800000;
+    while (n) {
+        if (n & 0x1) {
+            r = fmul32(r, x);
+            n -= 1;
+        } else {
+            x = fmul32(x, x);
+            n >>= 1;
+        }
+    }
+    return r;
+}
+
+// n!
+int factorial(int n) {
+    int r = 0x3f800000;
+    for (int i = 1; i <= n; i++) {
+        r = fmul32(r, i2f32(i));
+    }
+    return r;
+}
+
+// Sine by Taylor series
+int mySin(int x) {
+    int r = 0;
+    for (int n = 0; n < 5;
+         n++) {  // You can adjust the number of terms for accuracy
+        int k = f2i32(fadd32(fmul32(i2f32(2), i2f32(n)), i2f32(1)));
+        int s = 1 ^ ((-2) & -(n & 0x1));
+        r = fadd32(r, fdiv32(fmul32(i2f32(s), myPow(x, k)), factorial(k)));
+    }
+    return r;
+}
+
+int main() {
+    int sinev = mySin(0x3f490fdb);  // Calculate sine
+    return sinev;
+}
